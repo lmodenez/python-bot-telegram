d2530ab51b8ad83f8b8ac4b81264212196fdb6ee
lmodenez <lmodenez98@gmail.com>
2021-02-17 21:52:35 -0300

    use my token

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from telegram.ext import Updater
-import estoque, principal
+import estoque
+import principal
 
 
 def create_app():
 
     # create bot
-    token = "1537564807:AAEJnfoye2C4dYcSqukYKtBrZAxn_Vt8UJY"
+    token = "1670031977:AAHT9urvoMRj741IJ1_9BQtrH-0cr4uuVtk"
     updater = Updater(token=token, use_context=True)
 
-
-    #initiating extensions
+    # initiating extensions
     estoque.init_app()
     principal.init_app(updater)
 
-
     # initiating handles
 
-
-
-    #start app
+    # start app
     updater.start_polling()
+
 
 if __name__ == '__main__':
     create_app()
